ff1b1677bce8ece39ebf2ced88a7c88ea96ee41d
GiulianaMiret <dariela_77@hotmail.com>
2018-04-08 18:57:41 -0300

    PersistenciaSQLServer

---
+++ Persistencia/PersistenciaSQLServer/SQLServerFuenteDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.PersistenciaSQLServer
+{
+    class SQLServerFuenteDAO //: IFuenteDAO
+    {
+        /* public void Inserar(FuenteDTO pFuente) {}
+          
+        public FuenteDTO Buscar(int pIdFuente) {}
+          
+        public void Modificar(int pIdFuente, FuenteDTO pFuente) {}
+          
+        public void Eliminar(int pIdFuente) {}
+         */
+    }
+}
