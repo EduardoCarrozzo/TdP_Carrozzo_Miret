603201d661b9c19f52f60b0394c30280bafa3b2b
carrozzo.eduardo <carrozzo.eduardo@outlook.com>
2018-04-11 19:37:47 -0300

    Exception

---
+++ Persistencia/DAOException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    /// Clase pública DAOException.
+    /// Lanza las excepciones correspondientes en el uso de de los objetos de Email y Cuenta.
+    public class DAOException : Exception
+    {
+        // Constructor de la clase ExportadoresException
+        public DAOException(string pMensaje)
+            : base(pMensaje)
+        {
+        }
+    }
+}
